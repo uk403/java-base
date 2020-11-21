604f7c69d408a227a35a4c04831a888973688687
ukyu <ukyu233@gmail.com>
2020-11-21 10:45:27 +0800

    添加 工厂模式

---
+++ src/com/ukyu/designpatterns/FactoryMethod.java
@@ -0,0 +1,8 @@
+package com.ukyu.designpatterns;
+
+/**
+ * @author ukyu
+ * @date 2020/11/20
+ **/
+public class FactoryMethod {
+}
--- src/com/ukyu/designpatterns/SimpleFactoryMethod.java
+++ src/com/ukyu/designpatterns/SimpleFactoryMethod.java
@@ -1,9 +1,30 @@
 package com.ukyu.designpatterns;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+
 /**
- * 程序员、产品经理
+ * 以程序员、产品经理为例子
  *
- * 简单工厂模式
+ * 简单工厂模式（静态工厂方法模式）
+ *
+ * 优点： 1.对象的创建与对象的使用分离，将对象的创建交给专门的工厂类负责；  解耦*
+ *       2. 不必记住具体产品类的类名，只需要知道具体产品类对应的参数即可，
+ *          可以考虑使用全局变量，final static String xxx = ""; 对于复杂的类名，可以减少使用者的记忆量；
+ *       3. 当使用外部配置文件来管理这些参数时，例如 xml、properties，
+ *           还可以不改变代码，只需改变外部配置文件，就可以改变具体的产品类
+ *
+ * 缺点： 1. 当工厂类不正常工作时，整个系统都会受到影响
+ *       2. 当产品类增加时，就必须修改工厂类的逻辑；产品类不停的创建，工厂类的逻辑会变得复杂，不利于系统的扩展性与维护性；
+ *
+ * 适用的场合： 1. 需要创建的对象较少，这样工厂类的逻辑就不会太复杂；
+ *             2. 使用者不关心对象的创建过程，只需拿到该对象
+ *
+ * 模式的应用：
+ *
  * @author ukyu
  * @date 2020/11/19
  **/
@@ -14,6 +35,9 @@ public class SimpleFactoryMethod {
         Worker worker1 = Boss.getWorker("ProductManager");
         worker1.doWhat();
 
+        // 模式的应用
+//        DateFormat format =  DateFormat.getDateInstance();
+//        new ThreadPoolExecutor();
 
     }
 }
@@ -62,3 +86,5 @@ class Boss{
 //        }
     }
 }
+
+
