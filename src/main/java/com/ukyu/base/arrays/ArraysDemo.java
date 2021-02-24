cfdad3866ac949e0674deb95342a9d4daa19544a
ukyu <ukyu233@gmail.com>
2021-02-24 17:08:02 +0800

    add Arrays的探究

--- src/main/java/com/ukyu/base/arrays/ArraysDemo.java
+++ src/main/java/com/ukyu/base/arrays/ArraysDemo.java
@@ -1,10 +1,34 @@
 package com.ukyu.base.arrays;
 
+import lombok.extern.log4j.Log4j;
+
+import java.util.Arrays;
+import java.util.function.IntFunction;
+
 /**
  * Arrays
  * @author ukyu
  * @date 2021/2/23
  **/
+@Log4j
 public class ArraysDemo {
+    public static void main(String[] args) {
+        Integer[] a = {1, 2, 0, 21, 3, 6};
+//        排序算法，后面好好研究下
+//        Arrays.sort();
+//        利用ForkJoinPool的思想，后期研究
+//        Arrays.parallelSort(a);
 
+//        遍历整个数组，对当前索引前一个的值与当前索引的值进行操作，然后覆盖当前索引的值
+//        Arrays.parallelPrefix(a, (x, y) -> x + y);
+
+        log.info(Arrays.hashCode(a));
+
+//        遍历整个数组，对当前索引进行操作，然后覆盖当前索引的值
+        Arrays.parallelSetAll(a, x -> x*4);
+
+        Arrays.stream(a).forEach(x -> System.out.print(x + " "));
+
+
+    }
 }
