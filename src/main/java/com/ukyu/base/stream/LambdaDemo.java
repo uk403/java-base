d139441bf6e76eb855e790ace6a7ce667e1ad51b
ukyu <ukyu233@gmail.com>
2021-02-25 18:04:01 +0800

    add lambda

---
+++ src/main/java/com/ukyu/base/stream/LambdaDemo.java
@@ -0,0 +1,75 @@
+package com.ukyu.base.stream;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+/**
+ * lambda表达式
+ * 参考 https://docs.oracle.com/javase/tutorial/java/javaOO/lambdaexpressions.html
+ *
+ * @author ukyu
+ * @date 2021/2/25
+ **/
+public class LambdaDemo {
+    public static void main(String[] args) {
+//        匿名类看起来很丑，lambda表达式让它看起来更加的紧凑(compactly)
+//        functional interface是只包含一个抽象方法的接口;
+//        一个functional interface可以包含一个或多个默认方法或静态方法
+//        只有一个抽象方法，当你实现它时，可以省略其方法名，就可以使用lambda表达式
+        List<Person> roster = new ArrayList<>(10);
+        processPersonsWithFunction(
+                roster,
+                p -> p.getGender() == Person.Sex.MALE
+                        && p.getAge() >= 18
+                        && p.getAge() <= 25,
+                p -> p.getEmailAddress(),
+                email -> System.out.println(email)
+        );
+//       聚合操作替换这些action
+        roster.stream()
+                .filter(
+                        p -> p.getGender() == Person.Sex.MALE
+                                && p.getAge() >= 18
+                                && p.getAge() <= 25)
+                .map(p -> p.getEmailAddress())
+                .forEach(email -> System.out.println(email));
+
+
+
+    }
+
+    public static void processPersonsWithFunction(
+            List<Person> roster,
+            Predicate<Person> tester,
+            Function<Person, String> mapper,
+            Consumer<String> block) {
+        for (Person p : roster) {
+            if (tester.test(p)) {
+                String data = mapper.apply(p);
+                block.accept(data);
+            }
+        }
+    }
+}
+@Data
+class Person {
+
+    public enum Sex {
+        MALE, FEMALE
+    }
+
+    String name;
+    LocalDate birthday;
+    Sex gender;
+    String emailAddress;
+    int age;
+    public void printPerson() {
+        // ...
+    }
+}
---
+++ src/main/java/com/ukyu/base/stream/LambdaScopeTest.java
@@ -0,0 +1,45 @@
+package com.ukyu.base.stream;
+
+import java.util.function.Consumer;
+
+/**
+ * 访问封闭类中的局部变量
+ * @author ukyu
+ * @date 2021/2/25
+ **/
+public class LambdaScopeTest {
+
+    public int x = 0;
+
+    class FirstLevel {
+
+        public int x = 1;
+
+        void methodInFirstLevel(int x) {
+
+            // The following statement causes the compiler to generate
+            // the error "local variables referenced from a lambda expression
+            // must be final or effectively final" in statement A:
+//             x = 99;
+//            和匿名类一样，lambda表达式也只能访问final或最终final(没有改变它)的值
+
+            Consumer<Integer> myConsumer = (y) ->
+            {
+                System.out.println("x = " + x); // Statement A
+                System.out.println("y = " + y);
+                System.out.println("this.x = " + this.x);
+                System.out.println("LambdaScopeTest.this.x = " +
+                        LambdaScopeTest.this.x);
+            };
+
+            myConsumer.accept(x);
+
+        }
+    }
+
+    public static void main(String... args) {
+        LambdaScopeTest st = new LambdaScopeTest();
+        LambdaScopeTest.FirstLevel fl = st.new FirstLevel();
+        fl.methodInFirstLevel(23);
+    }
+}
