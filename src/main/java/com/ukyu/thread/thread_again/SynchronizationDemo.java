ad9a5790e6084bbd3b21a2257d3091783ffffc64
ukyu <ukyu233@gmail.com>
2021-03-04 00:12:55 +0800

    add 同步

---
+++ src/main/java/com/ukyu/thread/thread_again/SynchronizationDemo.java
@@ -0,0 +1,14 @@
+package com.ukyu.thread.thread_again;
+
+/**
+ * 同步； 线程通过访问共享的属性来进行交流；
+ * 这样会导致线程干扰(Thread interference)&内存一致性错误(memory consistency error)
+ * @author ukyu
+ * @time 2021/3/3 23:22
+ */
+public class SynchronizationDemo {
+//    线程干扰 https://docs.oracle.com/javase/tutorial/essential/concurrency/interfere.html
+
+//    内存一致性错误，避免其错误使用 happens-before 关系；
+
+}
--- src/main/java/com/ukyu/thread/thread_again/ThreadDemoX.java
+++ src/main/java/com/ukyu/thread/thread_again/ThreadDemoX.java
@@ -1,10 +1,30 @@
 package com.ukyu.thread.thread_again;
 
 /**
- *
  * @author ukyu
  * @time 2021/3/3 22:27
  */
 public class ThreadDemoX {
+    public static void main(String[] args) throws InterruptedException {
+//        1. interrupt
+//        会清除中断标志(interrupt flag)
+//        Thread.interrupted()
 
+//        非静态方法,不会清除
+//        Thread.currentThread().isInterrupted()
+
+//        InterruptedException被抛出时，中断标志也会被清除
+
+//        2. join, 谁调用这个方法，其他线程就要等待该线程执行完毕后才能执行
+//        Thread t1 = new Thread(() ->{
+//            for (int i = 0; i < 10; i++) {
+//                System.out.println(i);
+//            }
+//        });
+//        t1.start();
+//        t1.join();
+//        System.out.println("我要在最后打印~");
+
+
+    }
 }
